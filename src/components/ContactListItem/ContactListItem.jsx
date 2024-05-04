6fac248b9063af93451d8eb581f0ae662a8b5424
Olga <olyaruzhylenko@gmail.com>
2024-05-04 12:40:50 +0300

    delete -

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,11 +1,11 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch} from "react-redux";
 import {deleteContact} from '../../redux/contactsSlice'
 import css from "./ContactListItem.module.css";
 
 export default function ContactListItem({ name, number, id, onDelete }) {
   const dispatch = useDispatch();
   const handleDelete = () => dispatch(deleteContact(id))
-  return (
+  console.log(deleteContact(id));  return (
     <div>
       <h1>{name}</h1>
       <p>{number}</p>
